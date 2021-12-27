0237ed5f472e86d34ead4950909b050b129e0db6
OlZakh <olechka1945@ya.ru>
2021-12-27 22:57:30 +0300

    added build.gradle

---
+++ src/test/java/guru/qa/firstTest.java
@@ -0,0 +1,20 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class firstTest {
+@BeforeEach
+    void openYandex(){
+    System.out.println("open(\"ya.ru\")");
+    }
+
+    @Test
+    void firstTest(){
+
+    }
+
+}
+
