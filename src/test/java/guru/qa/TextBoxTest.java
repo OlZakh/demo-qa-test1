e6fa7bca84ca7eac1d8737606f8c15ce6f5b9dff
OlZakh <olechka1945@ya.ru>
2021-12-29 11:42:51 +0300

    добавлено сравнение полей

--- src/test/java/guru/qa/TextBoxTest.java
+++ src/test/java/guru/qa/TextBoxTest.java
@@ -1,11 +1,26 @@
 package guru.qa;
 
+import java.time.Duration;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x980";
+    }
+
     @Test
 
     void fullFormTest() {
@@ -14,6 +29,11 @@ public class TextBoxTest {
         $("[id=userEmail]").setValue("fenfff@ya.ru");
         $("#currentAddress").setValue("Moscow");
         $("#permanentAddress").setValue("penza");
+        $("#submit").click();
+        $("#output #name").shouldHave(text("Name"));
+        $("#output #email" ).shouldHave(text("fenfff@ya.ru"));
+        $("#output #currentAddress").shouldHave(text("Moscow"));
+        $("#output #permanentAddress").shouldHave(text("penza"));
 
     }
 }
